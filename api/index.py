25e0d31aa1c97389b58b83406a9930cef16636a3
Austyn Smock <67973162+AustynSmock@users.noreply.github.com>
2023-10-31 23:37:25 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(name)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-@socketio.on('connect')
-def test_connect(auth):
-    emit('my response', {'data': 'Connected'})
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    socketio.run(app)
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected!')
+
+if name == 'main':
+    socketio.run(app, debug=True)
