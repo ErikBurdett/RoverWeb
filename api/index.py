34fda0bd80ea3626ff10c45cce3915efe52788cb
Austyn Smock <67973162+AustynSmock@users.noreply.github.com>
2023-10-31 23:33:23 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+@socketio.on('connect')
+def test_connect(auth):
+    emit('my response', {'data': 'Connected'})
+
 if __name__ == '__main__':
     socketio.run(app)
