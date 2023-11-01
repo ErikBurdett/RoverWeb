d580619ad76b9596cc9ee9457197618988d32a8a
Austyn Smock <67973162+AustynSmock@users.noreply.github.com>
2023-10-31 23:10:51 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,10 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+correct_username = "rover"
+correct_password = "rover"
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == correct_username and password == correct_password:
+            return redirect(url_for('control_panel'))
+        else:
+            return render_template('login.html', error='Invalid credentials')
+
+    return render_template('login.html')
+
+@app.route('/control-panel')
+def control_panel():
+    return render_template('control_panel.html')
