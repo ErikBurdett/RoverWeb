f625fb5c14ff609861906baa074c1ad4a51e2777
Austyn Smock <67973162+AustynSmock@users.noreply.github.com>
2023-11-01 23:12:36 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('login.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # Add your login logic here (e.g., checking credentials)
+
+    return 'Logged in as ' + username
+
+if __name__ == '__main__':
+    app.run(ssl_context='adhoc', debug=True)
