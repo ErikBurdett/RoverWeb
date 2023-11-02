1aeea90c065ba552870e847518afc33044cc443a
Austyn Smock <67973162+AustynSmock@users.noreply.github.com>
2023-11-01 22:48:20 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,26 +1,55 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name)
 
-correct_username = "rover"
-correct_password = "rover"
+rover_message = ""
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+@app.route('/')
+def home():
+    return """
+    <main class="font-mono">
+      <h1 class="text-3xl text-center p-4 font-bold">Rover App</h1>
+      <div class='flex justify-center flex-col items-center'>
+        <button class='active:bg-neutral-500 border p-2 border-black rounded' onclick="fetchRover()">Call Rover</button>
+        <p class='text-blue-700 pt-4' id="rover-message"></p>
+        <div class="pt-4">
+          <input type="text" value="" id="new-message" placeholder="New message to Rover" class="border p-2 border-black rounded" />
+          <button class="active:bg-neutral-500 border p-2 border-black rounded ml-2" onclick="updateRoverMessage()">Send to Rover</button>
+        </div>
+      </div>
+    </main>
+    <script>
+      function fetchRover() {
+        fetch('/api/data')
+          .then(response => response.json())
+          .then(data => {
+            document.getElementById("rover-message").textContent = "Message from Rover: " + data.message;
+          });
+      }
 
-        if username == correct_username and password == correct_password:
-            return redirect(url_for('control_panel'))
-        else:
-            return render_template('login.html', error='Invalid credentials')
+      function updateRoverMessage() {
+        const newMessage = document.getElementById("new-message").value;
+        fetch('/api/data', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ text: newMessage }),
+        });
+      }
+    </script>
+    """
 
-    return render_template('login.html')
+@app.route('/api/data', methods=['GET', 'POST'])
+def api_data():
+    global rover_message
 
-@app.route('/control-panel')
-def control_panel():
-    return render_template('control_panel.html')
+    if request.method == 'GET':
+        return jsonify({'message': rover_message})
+    elif request.method == 'POST':
+        data = request.get_json()
+        rover_message = data['text']
+        return jsonify({'message': 'Message updated successfully'})
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.112', port=8080)
+    app.run(debug=True)
