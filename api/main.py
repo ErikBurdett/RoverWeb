869293d735b0a97a2fcdc8f1c6ea01ea4a242326
Austyn Smock <AustynSmock@gmail.com>
2023-11-29 02:15:48 -0600

    nice

--- api/main.py
+++ api/main.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
 
 app = Flask(__name__)
+
+app.config['SQALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY'] = "my super secret key"
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), nullable=False)
+  email = db.Column(db.String(100), nullable=False, unique=True)
+  date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def __repr__(self):
+    return '<Name %r>' % self.name
 
 #Form
 class NameForm(FlaskForm):
@@ -40,5 +55,5 @@ def name():
     name = form.name.data
     form.name.data = ''
     flash("Form Submitted Successfully!")
-    
+
   return render_template("name.html", name=name, form=form)
