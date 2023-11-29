04557b510017af803d8fc3ee917f8384acee4e00
Austyn Smock <AustynSmock@gmail.com>
2023-11-28 19:07:17 -0600

    nice
    nice

--- api/main.py
+++ api/main.py
@@ -4,5 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 
+#def index():
+#  return "<h1>Hello World!</h1>"
+
 def index():
-  return "<h1>Hello World?</h1>"
+  return render_template("index.htnml")
+
+@app.route('/user/<name>')
+
+def user(name):
+  return "<h1>Hello {}</h1>". format(name)
