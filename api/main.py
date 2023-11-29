1029e07c44f542a6a652707654bdacf59f9eb1c8
Austyn Smock <AustynSmock@gmail.com>
2023-11-28 20:03:08 -0600

    nice

--- api/main.py
+++ api/main.py
@@ -8,9 +8,19 @@ app = Flask(__name__)
 #  return "<h1>Hello World!</h1>"
 
 def index():
-  return render_template("index.html")
+  first_name = "Rowdy"
+  return render_template("index.html", first_name=first_name)
 
 @app.route('/user/<name>')
 
 def user(name):
-  return "<h1>Hello {}</h1>". format(name)
+  return render_template("user.html", username=name)
+
+#Error Pages
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+  return render_template("500.html"), 500
