95eb255e8b507c774d0f5d33616b24851ef6856d
Thaumcaster <nathanbradendayton@gmail.com>
2024-03-03 20:19:28 -0600

    Remove print, use flash

--- api/main.py
+++ api/main.py
@@ -148,7 +148,7 @@ def handle_data(data):
   data = request.json
   
   # Process the received data
-  print("Received data:", data)
+  flash(f"Received data: {data}")
   if data is None:
     data = "No data received"
   return render_template("user.html", data=data)
